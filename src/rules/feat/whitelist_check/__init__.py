10357dc9ba80d4805559e284d6025f850a33bfb4
Ernest Wong <ernestwongdexing@gmail.com>
2026-02-11 20:51:12 +0800

    integrated whitelist check with main code

--- src/rules/feat/whitelist_check/__init__.py
+++ src/rules/feat/whitelist_check/__init__.py
@@ -1,9 +1,9 @@
-from .whitelist_check import (
-    WhiteListCheckResults,
-    run_whitelist_check,
-    triggered_whitelistcheck,
-    whitelist_riskscore,
-    triggered_reason,
-    load_legit_domains_whitelist,
-    WHITELIST_SET,
-)
+# from .whitelist_check import (
+#     WhiteListCheckResults,
+#     run_whitelist_check,
+#     triggered_whitelistcheck,
+#     whitelist_riskscore,
+#     triggered_reason,
+#     load_legit_domains_whitelist,
+#     WHITELIST_SET,
+# )
